a6d102d840fd72a2ad5161fd298e05b9297a8367
zydgbbs <admin@zydgbbs.com>
2019-11-23 22:03:55 +0800

    LongAdder

---
+++ src/main/java/com/zydgbbs/thread/atomic/LongAdderTest.java
@@ -0,0 +1,10 @@
+package com.zydgbbs.thread.atomic;
+
+import java.util.concurrent.atomic.LongAdder;
+
+public class LongAdderTest {
+    public static void main(String[] args) {
+        LongAdder longAdder = new LongAdder();
+
+    }
+}
