bf9580c27c90af0f35a46a9661fe0b065b50b09c
zydgbbs <admin@zydgbbs.com>
2019-11-23 09:02:41 +0800

    synchronous container

---
+++ src/main/java/com/zydgbbs/thread/synchronouscontainer/HashTableTest.java
@@ -0,0 +1,4 @@
+package com.zydgbbs.thread.synchronouscontainer;
+
+public class HashTableTest {
+}
---
+++ src/main/java/com/zydgbbs/thread/synchronouscontainer/VectorTest.java
@@ -0,0 +1,13 @@
+package com.zydgbbs.thread.synchronouscontainer;
+
+import java.util.Vector;
+
+public class VectorTest {
+    public static void main(String[] args) {
+        Vector vector = new Vector();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        System.out.println(vector.size());
+    }
+}
