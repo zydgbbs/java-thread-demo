864b5e95523d65e2211db266f8e13f8766bc279a
zydgbbs <admin@zydgbbs.com>
2019-11-23 09:03:41 +0800

    concurrent container

---
+++ src/main/java/com/zydgbbs/thread/concurrentcontainer/CopyOnWriteArrayListTest.java
@@ -0,0 +1,11 @@
+package com.zydgbbs.thread.concurrentcontainer;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListTest {
+    public static void main(String[] args) {
+        //跟ArrayList的使用完全相同
+        CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
+
+    }
+}
