bb9ced7ed1c956fd08c9da071110741d246bfc22
zydgbbs <admin@zydgbbs.com>
2019-11-23 09:38:19 +0800

    WorkStealingPool

---
+++ src/main/java/com/zydgbbs/thread/pool/WorkStealingPoolTest.java
@@ -0,0 +1,75 @@
+package com.zydgbbs.thread.pool;
+
+import java.util.concurrent.*;
+
+public class WorkStealingPoolTest {
+    // 线程数
+    private static final int threads = 10;
+    // 用于计数线程是否执行完成
+    static CountDownLatch countDownLatch = new CountDownLatch(threads);
+
+    public static void main(String[] args) throws Exception{
+        test3();
+    }
+
+    public static void test1() throws Exception{
+        System.out.println("---- start ----");
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        for (int i = 0; i < threads; i++) {
+            executorService.execute(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                } catch (Exception e) {
+                    System.out.println(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        System.out.println("---- end ----");
+    }
+
+    public static void test2() throws InterruptedException{
+        System.out.println("---- start ----");
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        for (int i = 0; i < threads; i++) {
+//            Callable 带返回值
+            executorService.submit(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(Thread.currentThread().getName());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                }
+            }));
+        }
+        countDownLatch.await();
+        System.out.println("---- end ----");
+    }
+
+    public static void test3() throws Exception{
+        System.out.println("---- start ----");
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        for (int i = 0; i < threads; i++) {
+//          Runnable 带返回值
+            FutureTask<?> futureTask = new FutureTask<>(new Callable<String>() {
+                /**
+                 * call
+                 * @return currentThreadName
+                 */
+                @Override
+                public String call() {
+                    return Thread.currentThread().getName();
+                }
+            });
+            executorService.submit(new Thread(futureTask));
+            System.out.println(futureTask.get());
+        }
+        System.out.println("---- end ----");
+    }
+}
